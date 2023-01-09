19f3b4e6a7b4069d4af99c798d9701a413f3d5cb
Lewis Reid <lreidwilliams@gmail.com>
2023-01-09 20:23:36 +1000

    implemented basic RPN calculator

--- lib/reverse_polish.ex
+++ lib/reverse_polish.ex
@@ -1,18 +1,62 @@
 defmodule ReversePolish do
   @moduledoc """
-  Documentation for `ReversePolish`.
+  Reverse Polish Notation is a notation for simplifying expressions by eliminating the need for brackets.
   """
 
   @doc """
-  Hello world.
+  Calculates the value of an expression given in Reverse Polish Notation.
 
   ## Examples
 
-      iex> ReversePolish.hello()
-      :world
+      iex> ReversePolish.calculate("1 1 +")
+      2
+
+      iex> ReversePolish.calculate("2 4 * 8 +")
+      16
+
+      iex> ReversePolish.calculate("2 4 8 + *")
+      24
+
+      iex> ReversePolish.calculate("3 2 * 11 -")
+      -5
+
+      iex> ReversePolish.calculate("2 5 * 4 + 3 2 * 1 + /")
+      2
+
+      iex> ReversePolish.calculate("3 3 @")
+      ** (RuntimeError) Invalid token in expression: '@'
 
   """
-  def hello do
-    :world
+  def calculate(str) do
+    String.split(str)
+      |> Enum.reduce([], fn item, stack -> calculate(item, stack) end)
+      |> List.first
+  end
+
+  defp calculate("+", [first, second | stack]) do
+    [first + second | stack]
+  end
+
+  defp calculate("-", [first, second | stack]) do
+    [second - first | stack]
+  end
+
+  defp calculate("*", [first, second | stack]) do
+    [first * second | stack]
+  end
+
+  defp calculate("/", [first, second | stack]) do
+    [trunc(second / first) | stack]
+  end
+
+  defp calculate(operand, stack) when is_number(operand) do
+    [operand | stack]
+  end
+
+  defp calculate(operand, stack) do
+    case Integer.parse(operand) do
+      {n, _} -> calculate(n, stack)
+      :error -> raise "Invalid token in expression: '#{operand}'"
+    end
   end
 end
--- test/reverse_polish_test.exs
+++ test/reverse_polish_test.exs
@@ -1,8 +1,4 @@
 defmodule ReversePolishTest do
   use ExUnit.Case
   doctest ReversePolish
-
-  test "greets the world" do
-    assert ReversePolish.hello() == :world
-  end
 end
